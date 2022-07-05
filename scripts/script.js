43e8d46099b8aa2b59b878d3380fd5886891433e
duongand <duonganderson@gmail.com>
2022-07-04 21:14:36 -0400

    Local storage

--- scripts/script.js
+++ scripts/script.js
@@ -7,50 +7,59 @@ document.getElementById('task').addEventListener('keypress', function (event) {
 document.getElementById('submit').addEventListener('click', appendTask);
 
 const taskList = document.getElementById('to-do-list');
-taskList.addEventListener('click', function (event) {
+taskList.addEventListener('click', (event) => {
     const target = event.target;
 
     switch (target.className) {
         case 'incomplete':
-            target.style.textDecoration = 'line-through';
             target.className = 'completed';
+            window.localStorage.setItem(target.innerText, 'completed');
             break;
         case 'completed':
-            target.style.removeProperty('text-decoration');
             target.className = 'incomplete';
+            window.localStorage.setItem(target.innerText, 'incomplete');
             break;
         case 'remove':
             target.parentElement.remove();
+            window.localStorage.removeItem(target.previousSibling.innerText);
             break;
         default:
             break;
     };
 });
 
-function appendTask() {
-    const taskSubmission = document.getElementById('task');
-
-    if (taskSubmission.value === '') return;
+function createTaskElement(taskSubmission, status) {
 
     const taskButton = document.createElement('button');
     taskButton.setAttribute('class', 'remove');
     taskButton.innerHTML = 'X';
 
     const taskListElement = document.createElement('li');
-    taskListElement.setAttribute('class', taskSubmission.value);
+    taskListElement.setAttribute('class', taskSubmission);
 
     const task = document.createElement('button');
-    task.setAttribute('class', 'incomplete');
-    task.innerHTML = taskSubmission.value;
+    task.setAttribute('class', status);
+    task.innerHTML = taskSubmission;
 
     taskListElement.appendChild(task);
     taskListElement.appendChild(taskButton);
-
     taskList.appendChild(taskListElement);
 
-    clearInput();
+    window.localStorage.setItem(taskSubmission, status);
 };
 
-function clearInput() {
+function appendTask() {
+    const taskSubmission = document.getElementById('task').value;
+
+    if (taskSubmission === '') return;
+
+    createTaskElement(taskSubmission, 'incomplete');
     document.getElementById('task').value = '';
+};
+
+if (window.localStorage.length > 0) {
+    for (let i = 0; i < window.localStorage.length; i++) {
+        let taskKey = window.localStorage.key(i);
+        createTaskElement(taskKey, window.localStorage.getItem(taskKey));
+    };
 };
